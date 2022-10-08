5d3ea1a05bc78f66f0750ff4e41d9f66d637cf7f
Hundiak Valeriia <114095623+pojuipolynu@users.noreply.github.com>
2022-10-08 19:26:41 +0300

    Update ex2_4.py

--- ex2_4.py
+++ ex2_4.py
@@ -1,27 +1,32 @@
 class Read:
     def __init__(self, filename):
-        with open(filename, 'r') as textfile:
-            self.text = textfile.read()
+        self.text = filename
 
     def letters_num(self):
         count = 0
-        for char in self.text:
-            if char.isalpha():
-                count += 1
+        self.text.seek(0)
+        for line in self.text:
+            count += len(line)
         return count
 
     def words_num(self):
-        count = self.text.count(' ')
-        if count == 0 and self.text[1].isalpha():
-            count = 1
-        return count + 1
+        count = 0
+        self.text.seek(0)
+        for line in self.text:
+            k = line.split(' ')
+            count += len(k)
+        return count
 
     def sent_num(self):
-        count = self.text.count('.')
-        if count == 0 and self.text[1].isalpha():
-            count = 1
+        count = 0
+        self.text.seek(0)
+        for line in self.text:
+            k = line.split('.')
+            count += len(k)
         return count
 
 
-file = Read('data.txt')
+text = open('data.txt')
+file = Read(text)
 print('Letters count:', file.letters_num(), '\nWords count:', file.words_num(), '\nSentences count:', file.sent_num())
+text.close()
