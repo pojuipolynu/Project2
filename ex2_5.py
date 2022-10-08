a1ad6bf9a1f96c5af982f38ed8c5149f093f2f64
Hundiak Valeriia <114095623+pojuipolynu@users.noreply.github.com>
2022-10-08 19:27:50 +0300

    Update ex2_5.py

--- ex2_5.py
+++ ex2_5.py
@@ -4,30 +4,48 @@ class Student:
         self.surname = surname
         self.rbn = rbn
         self.grades = grades
+        self.average = 0
+
+    def __str__(self):
+        return f'Student: {self.surname} {self.name[0]}\nRecord book number: {self.rbn}\nGrades: {self.grades} ' \
+               f'\nAverage score: {self.average}\n'
 
 
 class Group:
-    def high_grade(self, grade):
-        high = [0 for x in range(len(grade))]
-        for x, i in enumerate(grade):
-            high[x] = [round(sum(i.grades)/len(i.grades), 2), f'Student: {i.surname} {i.name[0]}.']
-        high.sort(reverse=True)
-        return high
+    def __init__(self):
+        self.student = []
 
-    def stud_info(self, grade):
-        k = ''
-        b = self.high_grade(grade)
-        for x in range(5):
-            k += f'{b[x][1]}\nAverage score: {b[x][0]}\n'
-        return k
+    def add_stud(self, a):
+        if not isinstance(a, Student) or a in self.student:
+            raise Exception('Given information is invalid')
+        elif len(self.student) > 20:
+            raise Exception('Group can contain no more than twenty students')
+        else:
+            self.student.append(a)
+        return self.student
+
+    def high_grade(self):
+        for x in self.student:
+            x.average = round(sum(x.grades)/len(x.grades), 2)
+        self.student.sort(key=lambda a: a.average, reverse=True)
+        return self.student[:5]
+
+    def print_score(self):
+        for x in self.high_grade():
+            print(x)
 
 
-students = []
-students.append(Student('Yulia', 'Ponomareva', '3905798684', (2, 4, 6, 1, 34, 6)))
-students.append(Student('Lesyk', 'Teliha', '23009459884', (3, 4, 12, 1, 4, 7)))
-students.append(Student('Nastusia', 'Ponomareva', '03457-034484', (3, 11, 0, 3, 3, 23)))
-students.append(Student('Anna', 'Schwetc', '320957301', (12, 41, 0, 0, 2, 6)))
-students.append(Student('Katria', 'Zhuk', '128349875', (1, 6, 6, 7, 22, 4)))
-students.append(Student('Tia', 'Nailo', '973658902', (11, 0, 16, 23, 45, 0)))
-grad = Group()
-print(grad.stud_info(students))
+student1 = Student('Yulia', 'Ponomareva', '3905798684', (2, 4, 6, 1, 34, 6))
+student2 = Student('Lesyk', 'Teliha', '23009459884', (3, 4, 12, 1, 4, 7))
+student3 = Student('Nastusia', 'Ponomareva', '03457-034484', (3, 11, 0, 3, 3, 23))
+student4 = Student('Anna', 'Schwetc', '320957301', (12, 41, 0, 0, 2, 6))
+student5 = Student('Katria', 'Zhuk', '128349875', (1, 6, 6, 7, 22, 4))
+student6 = Student('Tia', 'Nailo', '973658902', (11, 0, 16, 23, 45, 0))
+group = Group()
+group.add_stud(student1)
+group.add_stud(student2)
+group.add_stud(student3)
+group.add_stud(student4)
+group.add_stud(student5)
+group.add_stud(student6)
+print(group.print_score())
